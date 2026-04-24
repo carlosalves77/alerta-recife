eb4a91955910692774083f7f20adbfae96bb12bd
carlosalves77 <carlosaalves07@outlook.com>
2026-04-24 14:54:30 -0300

    first commit

---
+++ src/main/java/com/carldev/alerta_recife/AlertaRecifeApplication.java
@@ -0,0 +1,13 @@
+package com.carldev.alerta_recife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaRecifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaRecifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carldev/alerta_recife/AlertaRecifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carldev.alerta_recife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaRecifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
