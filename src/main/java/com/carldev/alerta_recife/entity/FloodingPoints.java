b1e98612b906812ffd5853dd3043e09993cccfc7
carlosalves77 <carlosaalves07@outlook.com>
2026-04-28 14:10:52 -0300

    feat: entity do flooding points para criação do banco de dados.

---
+++ src/main/java/com/carldev/alerta_recife/entity/FloodingPoints.java
@@ -0,0 +1,48 @@
+package com.carldev.alerta_recife.entity;
+
+
+import com.carldev.alerta_recife.utils.IntensityOfTheFlooding;
+import jakarta.persistence.*;
+import lombok.*;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "flooding_points")
+public class FloodingPoints {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String logger;
+
+    @Column(name = "reference_point")
+    private String referencePoint;
+
+    @Column(nullable = false)
+    private String neighborhood;
+
+    @Column(columnDefinition = "geometry(Point, 4326)", nullable = false)
+    private Point coordinates;
+
+    @Enumerated(EnumType.STRING)
+    private IntensityOfTheFlooding intensity;
+
+    private boolean active = true;
+
+    private LocalDateTime registryDate = LocalDateTime.now();
+
+    private LocalDateTime actualizationDate;
+
+    private Integer confirmationVotes = 0;
+
+
+}
