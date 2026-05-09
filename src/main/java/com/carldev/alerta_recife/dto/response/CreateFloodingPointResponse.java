1e67823eceaa89a4309d6159b0e5a055dbc4c02b
carlosalves77 <carlosaalves07@outlook.com>
2026-05-08 22:49:52 -0300

    feature: adicionando o campo dto response de descrição e rua.

--- src/main/java/com/carldev/alerta_recife/dto/response/CreateFloodingPointResponse.java
+++ src/main/java/com/carldev/alerta_recife/dto/response/CreateFloodingPointResponse.java
@@ -3,8 +3,11 @@ package com.carldev.alerta_recife.dto.response;
 import com.carldev.alerta_recife.utils.IntensityOfTheFlooding;
 
 public record CreateFloodingPointResponse(
+        Long id,
+        String street,
         String logger,
         String referencePoint,
+        String description,
         String neighborhood,
         Double latitude,
         Double longitude,
