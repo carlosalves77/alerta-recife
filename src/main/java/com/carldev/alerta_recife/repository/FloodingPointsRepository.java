bb1fb6ee86cad4fe802f526e9ac3dcac7115fa32
carlosalves77 <carlosaalves07@outlook.com>
2026-05-08 22:52:24 -0300

    refactor: foi refeito a query de encontrar pontos próximo e bloquear caso tenha 20 metros de distancia um de outro, também foi adicionado a query de confirmar votos de um ponto de alagamento.

--- src/main/java/com/carldev/alerta_recife/repository/FloodingPointsRepository.java
+++ src/main/java/com/carldev/alerta_recife/repository/FloodingPointsRepository.java
@@ -2,9 +2,11 @@ package com.carldev.alerta_recife.repository;
 
 import com.carldev.alerta_recife.entity.FloodingPoints;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,14 +19,20 @@ public interface FloodingPointsRepository extends JpaRepository<FloodingPoints, 
     List<FloodingPoints> findFloodingPoints();
 
     @Query(value = "SELECT * FROM flooding_points l WHERE " +
-            "ST_DWithin(l.coordinates::geography, ST_SetSRID(ST_Point(:lon, :lat), 4326)::geography, " +
-            ":distance)",
+            "ST_DWithin(" +
+            "  l.coordinates::geography, " +
+            "  ST_GeographyFromText('POINT(' || :lon || ' ' || :lat || ')'), " +
+            "  :distance" +
+            ") LIMIT 1",
             nativeQuery = true)
     Optional<FloodingPoints> findNearByActive(
             @Param("lat") double lat,
             @Param("lon") double lon,
             @Param("distance") double distanceInMeters);
 
-
+    @Modifying
+    @Transactional
+    @Query("UPDATE FloodingPoints f SET f.confirmationVotes = confirmationVotes +1 WHERE f.id = :id")
+    void incrementConfirmationVotes(@Param("id") Long id);
 
 }
