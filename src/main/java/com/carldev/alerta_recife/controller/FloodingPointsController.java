3bb564b5685f3a0885c743403b4650eaed91a18c
carlosalves77 <carlosaalves07@outlook.com>
2026-05-09 23:37:36 -0300

    feature: criados os metodos controller de adicionar votos no ponto de alagamento, deletar todas os pontos(Apenas teste), deletar ponto pelo id, e atualizar a intensidade

--- src/main/java/com/carldev/alerta_recife/controller/FloodingPointsController.java
+++ src/main/java/com/carldev/alerta_recife/controller/FloodingPointsController.java
@@ -1,16 +1,21 @@
 package com.carldev.alerta_recife.controller;
 
-import com.carldev.alerta_recife.dto.response.CreateFloodingPointResponse;
 import com.carldev.alerta_recife.dto.request.CreateFloodingPointRequest;
+import com.carldev.alerta_recife.dto.request.UpdateIntensityFloodingPointRequest;
+import com.carldev.alerta_recife.dto.response.CreateFloodingPointResponse;
 import com.carldev.alerta_recife.dto.response.GetAllFloodingPointResponse;
+import com.carldev.alerta_recife.dto.response.UpdateConfirmationVotesFloodingPointResponse;
+import com.carldev.alerta_recife.dto.response.UpdateIntensityFloodingPointResponse;
 import com.carldev.alerta_recife.service.FloodingPointsService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/flooding")
 public class FloodingPointsController {
@@ -41,4 +46,45 @@ public class FloodingPointsController {
 
         return ResponseEntity.status(HttpStatus.OK).body(pointResponse);
     }
+
+    @PatchMapping("/{id}/intensity")
+    public ResponseEntity<UpdateIntensityFloodingPointResponse> updateIntensityFloodingPoint(
+            @Valid
+            @PathVariable(value = "id") Long id,
+            @RequestBody UpdateIntensityFloodingPointRequest intensity
+    ) {
+
+        UpdateIntensityFloodingPointResponse response =
+                floodingPointsService.updateIntensityFloodingPoint(id, intensity.intensity());
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteFloodingPointById(
+            @PathVariable Long id
+    ) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(floodingPointsService.deleteFloodingPointById(id));
+    }
+
+    @PatchMapping("/{id}/votes")
+    public ResponseEntity<UpdateConfirmationVotesFloodingPointResponse> updateConfirmationVotes(
+            @PathVariable("id") Long id
+    ) {
+
+        UpdateConfirmationVotesFloodingPointResponse response = floodingPointsService.
+                updateConfirmationVotesFloodingPointResponse(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/all")
+    public ResponseEntity<String> deleteAllFloodingPoint() {
+        floodingPointsService.deleteAllFloodingPoint();
+
+        return ResponseEntity.status(HttpStatus.OK).body("Todos os pontos de alagamentos deletados");
+    }
+
 }
