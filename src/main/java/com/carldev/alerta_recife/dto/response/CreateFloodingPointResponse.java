05522e9ca983a361c99db9f2eec2d9a9f525aaa4
carlosalves77 <carlosaalves07@outlook.com>
2026-05-11 22:40:18 -0300

    feature: adicionando campo de adicionar imagens no dto de response de ponto de alagamento.

--- src/main/java/com/carldev/alerta_recife/dto/response/CreateFloodingPointResponse.java
+++ src/main/java/com/carldev/alerta_recife/dto/response/CreateFloodingPointResponse.java
@@ -2,6 +2,8 @@ package com.carldev.alerta_recife.dto.response;
 
 import com.carldev.alerta_recife.utils.IntensityOfTheFlooding;
 
+import java.util.List;
+
 public record CreateFloodingPointResponse(
         Long id,
         String street,
@@ -12,7 +14,8 @@ public record CreateFloodingPointResponse(
         Double latitude,
         Double longitude,
         IntensityOfTheFlooding intensity,
-        Integer confirmationVotes
+        Integer confirmationVotes,
+        List<String> images
 ) {
 
 }
