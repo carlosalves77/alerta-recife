91df802f193095c9e631a98b26999727e82c5c05
carlosalves77 <carlosaalves07@outlook.com>
2026-05-11 22:42:02 -0300

    feature: criação da entity de imagens com a ligação do ponto de alagamento.

---
+++ src/main/java/com/carldev/alerta_recife/entity/FloodingPointImage.java
@@ -0,0 +1,26 @@
+package com.carldev.alerta_recife.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "flooding_point_images")
+public class FloodingPointImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "flooding_point_id", nullable = false)
+    private FloodingPoints floodingPoint;
+}
