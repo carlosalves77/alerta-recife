bfb733584a94a1b77654c69620fc823e585663b5
carlosalves77 <carlosaalves07@outlook.com>
2026-05-12 21:13:58 -0300

    feature: adicionando campo para pegar a lista de imagens

--- src/main/java/com/carldev/alerta_recife/mapper/GetFloodingPointsMapper.java
+++ src/main/java/com/carldev/alerta_recife/mapper/GetFloodingPointsMapper.java
@@ -1,24 +1,22 @@
 package com.carldev.alerta_recife.mapper;
 
 import com.carldev.alerta_recife.dto.response.GetAllFloodingPointResponse;
+import com.carldev.alerta_recife.entity.FloodingPointImage;
 import com.carldev.alerta_recife.entity.FloodingPoints;
+
+import java.util.List;
 
 public class GetFloodingPointsMapper {
 
     public static GetAllFloodingPointResponse toDto(FloodingPoints floodingPoints) {
 
-        return new GetAllFloodingPointResponse(
-                floodingPoints.getId(),
-                floodingPoints.getStreet(),
-                floodingPoints.getLogger(),
-                floodingPoints.getReferencePoint(),
-                floodingPoints.getNeighborhood(),
-                floodingPoints.getDescription(),
-                floodingPoints.getCoordinates().getY(),
-                floodingPoints.getCoordinates().getX(),
-                floodingPoints.getIntensity(),
-                floodingPoints.getConfirmationVotes()
-        );
+        List<String> urls = floodingPoints.getImages().stream().map(FloodingPointImage::getImageUrl).toList();
+
+        return new GetAllFloodingPointResponse(floodingPoints.getId(), floodingPoints.getStreet(),
+                floodingPoints.getLogger(), floodingPoints.getReferencePoint(),
+                floodingPoints.getNeighborhood(), floodingPoints.getDescription(),
+                floodingPoints.getCoordinates().getY(), floodingPoints.getCoordinates().getX(),
+                floodingPoints.getIntensity(), floodingPoints.getConfirmationVotes(), urls);
     }
 
 
