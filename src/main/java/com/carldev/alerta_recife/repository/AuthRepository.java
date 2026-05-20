bc4c04a2b3d2ec8a0c5bfc827df9ea49f5220dca
carlosalves77 <carlosaalves07@outlook.com>
2026-05-20 20:01:31 -0300

    feat: criação do repository para o auth user, foi feito query para achar usuário pelo email, verificar se e-mail existe, e fazer um update do último login ao logar.

---
+++ src/main/java/com/carldev/alerta_recife/repository/AuthRepository.java
@@ -0,0 +1,25 @@
+package com.carldev.alerta_recife.repository;
+
+import com.carldev.alerta_recife.entity.UserAuth;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AuthRepository extends JpaRepository<UserAuth, UUID> {
+
+    Optional<UserAuth> findUserByEmail(String username);
+
+    boolean existsByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE UserAuth u SET u.lastLoginAt = :lastLogin WHERE u.id = :id")
+    void updateLastLoginById(@Param("id") UUID id,
+                            @Param("lastLogin") Instant lastLogin);
+}
