a70d208198a9f21b9e329c680f8e383a0d4ebc1e
carlosalves77 <carlosaalves07@outlook.com>
2026-05-20 19:59:18 -0300

    feat: criação mapper para auth user

---
+++ src/main/java/com/carldev/alerta_recife/mapper/AuthMapper.java
@@ -0,0 +1,29 @@
+package com.carldev.alerta_recife.mapper;
+
+import com.carldev.alerta_recife.dto.request.AuthRegisterRequest;
+import com.carldev.alerta_recife.dto.response.AuthRegisterResponse;
+import com.carldev.alerta_recife.entity.UserAuth;
+
+public class AuthMapper {
+
+    public static AuthRegisterResponse toDto(UserAuth userAuth) {
+
+        return new AuthRegisterResponse(
+                userAuth.getEmail(),
+                userAuth.getUsername(),
+                userAuth.getCreatedAt(),
+                userAuth.getRole()
+        );
+    }
+
+    public static UserAuth toEntity(AuthRegisterRequest authRegisterRequest) {
+
+        return UserAuth.builder()
+                .email(authRegisterRequest.email())
+                .password(authRegisterRequest.password())
+                .username(authRegisterRequest.username())
+                .build();
+    }
+
+
+}
