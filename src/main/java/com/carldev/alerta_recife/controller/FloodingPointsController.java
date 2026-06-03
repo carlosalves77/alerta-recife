a0e16cdb4c815bac01307d29d8e55bc79ab9869e
carlosalves77 <carlosaalves07@outlook.com>
2026-06-03 15:21:58 -0300

    feat: controller para o ponto de alagamento.

--- src/main/java/com/carldev/alerta_recife/controller/FloodingPointsController.java
+++ src/main/java/com/carldev/alerta_recife/controller/FloodingPointsController.java
@@ -6,12 +6,14 @@ import com.carldev.alerta_recife.dto.response.CreateFloodingPointResponse;
 import com.carldev.alerta_recife.dto.response.GetAllFloodingPointResponse;
 import com.carldev.alerta_recife.dto.response.UpdateConfirmationVotesFloodingPointResponse;
 import com.carldev.alerta_recife.dto.response.UpdateIntensityFloodingPointResponse;
+import com.carldev.alerta_recife.entity.UserAuth;
 import com.carldev.alerta_recife.service.FloodingPointsService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -33,11 +35,12 @@ public class FloodingPointsController {
     public ResponseEntity<CreateFloodingPointResponse> createFloodingPoint(
             @Valid
             @RequestPart("data") CreateFloodingPointRequest request,
-            @RequestPart("file") List<MultipartFile> files
+            @RequestPart("file") List<MultipartFile> files,
+            Authentication authentication
     ) throws IOException {
 
         CreateFloodingPointResponse createFloodingPointResponse = floodingPointsService.createFloodingPoint(
-                request, files
+                request, files, authentication
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createFloodingPointResponse);
@@ -67,11 +70,40 @@ public class FloodingPointsController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteFloodingPoint(
-            @PathVariable Long id
-    ) {
+            @PathVariable Long id,
+            Authentication authentication
+    ) throws IllegalAccessException {
 
-        return ResponseEntity.status(HttpStatus.OK).body(floodingPointsService.deleteFloodingPoint(id));
+        UserAuth userAuth = (UserAuth) authentication.getPrincipal();
+
+        if (userAuth == null) {
+            throw new IllegalAccessException("Usuário invalido");
+        }
+
+        String floodingPoints = floodingPointsService.deleteFloodingPoint(id,
+                userAuth.getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(floodingPoints);
     }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<GetAllFloodingPointResponse>> getFloodingPointsByUser(
+            Authentication authentication
+    ) throws IllegalAccessException {
+
+        UserAuth userAuth = (UserAuth) authentication.getPrincipal();
+
+        if (userAuth == null) {
+            throw new IllegalAccessException("Usuário invalido");
+        }
+
+        List<GetAllFloodingPointResponse> listFloodingPoints =
+                floodingPointsService.getFloodingPointsByUserId(userAuth.getId());
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(listFloodingPoints);
+    }
+
 
     @PatchMapping("/{id}/votes")
     public ResponseEntity<UpdateConfirmationVotesFloodingPointResponse> updateConfirmationVotes(
