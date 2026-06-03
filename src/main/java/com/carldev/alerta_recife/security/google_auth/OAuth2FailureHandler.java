390d45d9cac53eeebb8d9973974034fe31f8ed24
carlosalves77 <carlosaalves07@outlook.com>
2026-06-03 15:31:28 -0300

    feat: adicionado oauth2 handle em caso de falhas no login

---
+++ src/main/java/com/carldev/alerta_recife/security/google_auth/OAuth2FailureHandler.java
@@ -0,0 +1,26 @@
+package com.carldev.alerta_recife.security.google_auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception)
+            throws IOException, ServletException {
+
+        getRedirectStrategy().sendRedirect(
+                request, response,
+                "https://alertarecife.carldev.online/auth/callback?error=oauth2_failure"
+        );
+    }
+}
